1cf6ff56d8c9e9c301db47f07bf10c90520223b3
ivank27 <106963472+KoniG27@users.noreply.github.com>
2023-02-09 11:43:09 +0100

    Terminado

--- Semaforo/src/Main.java
+++ Semaforo/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
+    static int recurs = 3;
     public static void main(String[] args) {
-        RecursosControl recursosControl = new RecursosControl(20);
+
+        RecursosControl recursosControl = new RecursosControl(recurs);
         Proceso[] procesos = new Proceso[5];
 
         for (int i = 0; i < procesos.length; i++) {
@@ -29,17 +31,17 @@ class Proceso extends Thread {
 
     @Override
     public void run() {
-        int r = (int) (Math.random() * 5) + 1;
-        int l = (int) (Math.random() * 5) + 1;
+        int r = (int) (Math.random() * Main.recurs) + 1;
+        int l = r;
 
         try {
             System.out.println("Proceso " + procesoId + " esta intentando reservar " + r + " unidades");
             recursosControl.reserve(r);
             System.out.println("Proceso " + procesoId + " reservo " + r + " unidades");
             Thread.sleep((int) (Math.random() * 1000));
-            System.out.println("Proceso " + procesoId + " esta intentando liberar " + l + " unidades");
-            recursosControl.release(l);
             System.out.println("Proceso " + procesoId + " libero " + l + " unidades");
+            recursosControl.release(l);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
